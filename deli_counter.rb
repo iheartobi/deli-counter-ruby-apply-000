8d75472335dc67e4d646c94ebcd5878c850d67f4
Ititi Obidah <iobidah9@gmail.com>
2019-03-30 00:19:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   end
 end 
+def now_serving(katz_deli)
+  if katz_deli.empty? 
+    puts 
+end 
